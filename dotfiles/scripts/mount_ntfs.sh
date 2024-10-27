634e4295011898c59378c39f5cd07eff98cd6bb3
Kio <46105187+kio-grnd@users.noreply.github.com>
2024-10-27 08:55:59 -0300

    Delete dotfiles/scripts directory

--- dotfiles/scripts/mount_ntfs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Montar el dispositivo NTFS
-sudo mount /dev/sdb1 /mnt/ntfs-1
--- dotfiles/scripts/nvidia-fix-tearing.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# nVIDIA fix-tearing
-# LG sudo nvidia-settings --assign CurrentMetaMode="HDMI-0:1920x1080_60 +0+0 { ForceFullCompositionPipeline = On }"
-
-sudo nvidia-settings --assign "CurrentMetaMode=HDMI-0: nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
